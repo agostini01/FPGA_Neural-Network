fab530de8f5d91d4830684a846bf9cdf3760f787
Nicolas Agostini <n.b.agostini@gmail.com>
2014-11-30 13:24:05 -0200

    NN add fixed point extension

--- source_files/neural_net_pkg.vhd
+++ source_files/neural_net_pkg.vhd
@@ -5,8 +5,12 @@
 -- Coder       : Alexander H Pham (VHDL)
 -------------------------------------------------------
 library ieee;
-    use ieee.std_logic_1164.all;
-    use ieee.std_logic_unsigned.all;
+	use ieee.std_logic_1164.all;
+	use ieee.numeric_std.all;
+
+	use work.fixed_float_types.all; -- ieee_proposed for VHDL-93 version
+	use work.fixed_pkg.all; -- ieee_proposed for compatibility version
+
     
 package NN_PKG is
 
@@ -14,6 +18,13 @@ package NN_PKG is
 			constant INPUT_PERCEPTRONS 	: integer := 4;
 			constant HIDDEN_PERCEPTRONS 	: integer := 4;
 			constant OUT_PERCEPTRONS 		: integer := 4;
+			
+			type FIX_ARRAY is array (integer range <>) of sfixed;
+			constant FIX_SIZE 	: sfixed := to_sfixed(6.5 ,3,-3);
+
+
+
+			
 
 end;
 
