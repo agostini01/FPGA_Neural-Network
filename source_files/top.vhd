b8f26844a248c3c2bb3383b96dcc692b77cfe4c0
Nicolas Agostini <n.b.agostini@gmail.com>
2014-12-08 15:06:22 -0200

    NN: Fix Dataset problem

--- source_files/top.vhd
+++ source_files/top.vhd
@@ -1,9 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.NN_PKG.all;
 
 entity TOP is
 	port	(
-			START, CLK											:in std_logic;
+			START, CLK							:in std_logic;
 			IN1, IN2, IN3, IN4				:in integer range -5 to 5;
 			OUT1, OUT2, OUT3, OUT4 			:out integer
 			);
@@ -19,6 +20,21 @@ architecture STRUCTURE of TOP is
 				OUTPUT1, OUTPUT2, OUTPUT3, OUTPUT4			:out integer;
 				START, CLK											:in std_logic		
 				);
+	end component;
+	
+	component GENERIC_NEURAL_NET 
+		generic	(
+					N_I,N_H,N_O : natural -- N: Number of inputs; M: Number of Neurons/outputs
+					);
+		
+		port		(
+					INPUT													:in INT_ARRAY(0 to N_I);
+					CONTROL_IN, CONTROL_HIDDEN, CONTROL_OUT	:in std_logic;
+					OUTPUT												:out INT_ARRAY(0 to N_O);
+					START, CLK											:in std_logic;
+					W_B_ARRAY_3D: in FIX_ARRAY_3D(0 to (2))
+					);
+
 	end component;
 	
 	begin
